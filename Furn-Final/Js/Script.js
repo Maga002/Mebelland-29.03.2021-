3b49ed3212e0633874f04358e20631fcab249066
Maga002 <72382783+Maga002@users.noreply.github.com>
2021-03-29 02:13:37 +0400

    Add files via upload

---
+++ Furn-Final/Js/Script.js
@@ -0,0 +1,26 @@
+ $(".fff").slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 1000
+    
+ })
+ $(".eee").slick({
+     
+    infinite: true,
+    slidesToShow: 4,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    
+ })
+ $(".mmm").slick({
+    arrows:false,
+   infinite: true,
+   slidesToShow: 5,
+   centerMode: true,
+   centerPadding: '60px',
+   autoplay: true,
+    autoplaySpeed: 1000,
+    dots:false
+ })
+
+
---
+++ Furn-Final/Js/dist/Script.dev.js
@@ -0,0 +1,14 @@
+"use strict";
+
+$(".fff").slick({
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 1000
+});
+$(".eee").slick({
+  infinite: true,
+  slidesToShow: 4,
+  slidesToScroll: 2,
+  autoplay: true,
+  autoplaySpeed: 2000
+});
